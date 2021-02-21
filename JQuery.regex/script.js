6f9d5afda35f6a965f84e212f66fb6a90fe5fec6
MrLayerGame <tomas.bezruchko@gmail.com>
2021-02-21 18:50:38 +0200

    deleted shit

--- JQuery.regex/script.js
+++
@@ -1,8 +0,0 @@
-let regex = /\w+@[a-z]{2,5}\.[a-z]+/g;
-
-
-$(".button").click(() =>{
- alert(regex. test($("input").val()));
-});
-
-
--- JQuery/script.js
+++
@@ -1,7 +0,0 @@
-let regex = /\w+@[a-z]{2,5}\./g;
-
-$("button").click(()=>{
-
-    alert( regex.test( $("input").val() ) );
-
-});
